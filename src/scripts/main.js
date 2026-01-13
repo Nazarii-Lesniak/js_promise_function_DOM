c369c54f1de900223356a5db711f7799e3f90ced
Nazarii Lesniak <naziksay@gmail.com>
2026-01-13 16:45:37 +0200

    changed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,11 +2,11 @@
 
 function waitFor(element, eventName) {
   return new Promise((resolve) => {
-    function handler(elementListener) {
+    function handler(e) {
       element.removeEventListener(eventName, handler);
 
       resolve(
-        `It was ${eventName} on the element: ${element.nodeName}, id: ${element.id}`,
+        `It was ${eventName} on the element: ${element.nodeName}, id: ${element.id}.`,
       );
     }
 
